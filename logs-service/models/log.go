a0c1379ba37f775ea0e6b86ab80fc37aaf5d0dff
naufalyunan <naufalyunan45@gmail.com>
2024-12-02 20:23:08 +0700

    finish log service

--- logs-service/models/log.go
+++ logs-service/models/log.go
@@ -3,8 +3,8 @@ package models
 import "gorm.io/gorm"
 
 type ActivityLog struct {
-    gorm.Model
-    UserID        uint   `gorm:"not null"` // Foreign key to Users table
-    ActionType    string `gorm:"type:enum('swipe', 'purchase');not null"`
-    ActionDetails string `gorm:"type:text"`
+	gorm.Model
+	UserID        uint   `gorm:"not null"` // Foreign key to Users table
+	ActionType    string `gorm:"type:varchar(20);not null"`
+	ActionDetails string `gorm:"type:text"`
 }
