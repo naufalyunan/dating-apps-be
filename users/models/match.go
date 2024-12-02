7cf6d241c49231ff542ac0645c523508babdaa5a
naufalyunan <naufalyunan45@gmail.com>
2024-12-03 00:08:51 +0700

    checkpoint 2

--- users/models/match.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Match struct {
-    gorm.Model
-    User1ID   uint   `gorm:"not null"` // First user in the match
-    User2ID   uint   `gorm:"not null"` // Second user in the match
-}
